a1d40c5f7e8cc3a1f4d368d7a49f716c09c2f5da
Andrii <andrewchepeliuk@gmail.com>
2023-01-03 23:55:30 +0200

    get musi library data

---
+++ MyAmazonMusicLibraryApp/Models/SongEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace MyAmazonMusicLibraryApp.Models
+{
+    public class SongEntity
+    {
+        //Song name, Artist name, Album name, Duration
+        public string? SongName { get; set; }
+        public string? ArtistName { get; set; }
+        public string? AlbumName { get; set; }
+        public string? Duration { get; set; }
+    }
+}
---
+++ MyAmazonMusicLibraryApp/Views/SongsView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MyAmazonMusicLibraryApp.Views
+{
+    public partial class SongsView : UserControl
+    {
+        public SongsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
